273a0d08e7ff47baf284329552e1e3ddb0e6040a
Elderes <147399524+Elderes@users.noreply.github.com>
2024-03-25 21:59:28 -0300

    LinkedList

---
+++ LinkedList/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedListCustom linkedlist = new LinkedListCustom();
+
+        linkedlist.addLast(5);
+        linkedlist.addLast(4);
+        linkedlist.addLast(2);
+        linkedlist.addFirst(0);
+        linkedlist.print();
+    }
+}
