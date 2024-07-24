0326aea2f5888b49b566609f6c3b57ee84bdfad8
Elderes <abraao.borges@academico.ifpb.edu.br>
2024-07-24 11:43:51 -0300

    commit

--- HashTable/src/LinkedList.java
+++ HashTable/src/LinkedList.java
@@ -30,10 +30,18 @@ public class LinkedList {
         }
     }
 
-    public void exibir() {
-        while (next != null) {
-            System.out.println(head);
+    public boolean remover(int matricula) {
+        Node current = head;
+        Node previous = null;
+
+        while (current != null) {
+            if (current.aluno.getMatricula() == matricula) {
+                if (previous != null) {
+                    head = current.next;
+                } else {
+                    previous.next = current.next;
+                }
+            }
         }
-        System.out.println(head);
     }
 }
