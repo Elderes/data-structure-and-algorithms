c797a3746910813702ba7a3467029b50535de03c
Elderes <147399524+Elderes@users.noreply.github.com>
2024-07-24 07:47:28 -0300

    h

---
+++ HashTable/src/Aluno.java
@@ -0,0 +1,30 @@
+public class Aluno {
+    int matricula;
+    String nome;
+
+    public Aluno(int matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno(matrúcla = " + matricula + ", nome = " + nome + ")";
+    }
+}
---
+++ HashTable/src/LinkedList.java
@@ -0,0 +1,15 @@
+// Node and LinkedList
+
+class Node {
+    Aluno aluno;
+    Node next;
+
+    public Node(Aluno aluno) {
+        this.aluno = aluno;
+        this.next = null;
+    }
+}
+
+public class LinkedList {
+
+}
