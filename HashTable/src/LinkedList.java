1268e2b11d87e511e2511906a299006fd28ecbf9
Elderes <abraao.borges@academico.ifpb.edu.br>
2024-07-24 16:52:56 -0300

    commit

--- HashTable/src/LinkedList.java
+++ HashTable/src/LinkedList.java
@@ -34,12 +34,21 @@ public class LinkedList {
 
         while (current != null) {
             if (current.aluno.getMatricula() == matricula) {
-                if (previous != null) {
-                    head = current.next;
+                if (previous != null) { // Em caso de remoção do primeiro nó.
+                    head = current.next; // O segundo nó passa a ser a cabeça da lista.
                 } else {
-                    previous.next = current.next;
+                    previous.next = current.next; // O nó anterior é "amarrado" com o próximo.
                 }
+                return true; // Encontrou a matrícula. Quebra o ciclo.
             }
+            previous = current; // Mantem a referência do nó anterior se achar o elemento a ser removido.
+            current = current.next; // Vai para o próximo nó e repete o ciclo.
         }
+        return false; // Caso a matrícula não seja encontrada na lista.
     }
+
+    public String pegarValor(int matricula) {
+        Node current = head; //
+    }
+
 }
