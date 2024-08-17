8e90ffd8df48ab79d11831b9e27e3356c59fd7a3
Elderes <abraao.borges@academico.ifpb.edu.br>
2024-08-17 10:44:09 -0300

    rotação direita e esqeurda. inserção e balanceamento.

--- tree/avl_tree/AVLTree.java
+++ tree/avl_tree/AVLTree.java
@@ -11,12 +11,89 @@ class Node {
 }
 
 public class AVLTree {
-    Node root;
-
-    // Retorna a altura da árvore.
+    private Node root;
+    
+    /* Retorna a altura da árvore. */
     int height(Node n) {
-        if (n == null)
-            return 0;
-        return n.height;
+        return (n == null) ? 0 : n.height;
     }
+    
+    /* "(a > b)" é um boolean. "?" verifica o valor desse boolean,
+    e retorna "a" caso verdadeiro, e "b" caso falso. */
+    int max(int a, int b) {
+        return (a > b) ? a : b; 
+    }
+
+    void updateHeight(Node n) {
+        n.height = 1 + max(height(n.left), height(n.right));
+    }
+
+    int getBalance(Node n) {
+        return (n == null) ? 0 : height(n.right) - height(n.left);
+    }
+
+    // https://www.baeldung.com/java-avl-trees
+    Node rightRotate(Node y) {
+        Node x = y.left;
+        Node z = x.right; 
+
+        x.right = y;
+        y.left = z;
+
+        updateHeight(y);
+        updateHeight(x);
+        
+        return x;
+    }
+    
+    Node leftRotate(Node x) {
+        Node y = x.right;
+        Node z = y.left; 
+
+        y.left = x;
+        x.right = z;
+
+        updateHeight(y);
+        updateHeight(x);
+        
+        return x;
+    }
+
+    // Tendo como base subárvore esquerda - subárvore direita.
+    Node rebalance(Node z) {
+        updateHeight(z);
+        int balance = getBalance(z);
+
+        if (balance > 1) {
+            if (height(z.right.right) > height(z.right.left)) {
+                z = leftRotate(z);
+            } else {
+                z.right = rightRotate(z.right);
+                z = leftRotate(z);
+            }
+        } else if (balance < -1) {
+            if (height(z.left.left) > height(z.left.right)) 
+                z = rightRotate(z);
+            else {
+                z.left = leftRotate(z.left);
+                z = rightRotate(z);
+            }
+        }
+        return z;
+    }
+
+    Node insert(Node current, int key) {
+        if (current == null) {
+            return new Node(key);
+        } else if (current.key > key) {
+            current.left = insert(current.left, key);
+        } else if (current.key < key) {
+            current.right = insert(current.right, key);
+        } else {
+            throw new RuntimeException("Duplicate Key!");
+        }
+        return rebalance(current);
+    }
+
+    // Delete node.
 }
