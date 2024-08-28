ea2fe7ab52e4eb2eb1aaa17465e64c1e5b80f86a
Elderes <abraao.borges@academico.ifpb.edu.br>
2024-08-28 11:21:10 -0300

    Árvore B não finalizada ainda.

---
+++ tree/b_tree/BTree.java
@@ -0,0 +1,22 @@
+package tree.b_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+    LinkedList keys;
+    boolean isLeave;
+
+    Node(int key) {
+        this.key = new LinkedList();
+    }
+}
+
+public class BTree {
+    int order;
+
+Btree(int order ) {
+
+    }
+ 
+}   
